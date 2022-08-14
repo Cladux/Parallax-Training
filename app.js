62afb6dcdbabf431e446e57f68a0bbf390562cf4
Soroush Sharifi <sharifysoroush@gmail.com>
2022-08-14 21:50:19 +0430

    Compelet-hero_header-section--functions

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+const header = document.querySelector("header");
 const bgImg = document.querySelector(".hero__header_bg img");
 const bgTitle = document.querySelector(".hero__header_bg h1");
 const object = document.querySelector(".hero__header_object");
-
 document.addEventListener("scroll", () => {
   const yOff = window.pageYOffset;
   const END_OF_HERO_HEADER = 2000;
+  header.style.position = `fixed`;
 
   if (yOff < END_OF_HERO_HEADER) {
     const percent = yOff / END_OF_HERO_HEADER;
-    console.log(percent);
+    header.style.transform = `translate3d(0, 0, 0)`;
 
     bgImg.style.filter = `blur(${percent * 2}px)`;
     bgTitle.style.transform = `scale(${1 + percent * 0.3} , 1.3)`;
     object.style.transform = `scale(${1 + percent * 0.1})`;
   } else if (yOff > END_OF_HERO_HEADER) {
+    const offset = yOff - END_OF_HERO_HEADER;
+
     bgImg.style.filter = `blur(2px)`;
-    bgTitle.style.transform = `scale(1.3 , 2) translate(80px, 0) `;
-    bgTitle.style.transition = `all .5s ease-out`;
-    object.style.transform = `scale(1.08)`;
+    bgTitle.style.transform = `scale(1.3 , 1.3)`;
+    object.style.transform = `scale(1.1)`;
+    console.log(offset);
+    // header.style.position = `relative`;
+    header.style.transform = `translate3d(0, -${offset}px, 0)`;
+
+    // app.style.transform = `translateY(-${offset})`;
   }
 });
